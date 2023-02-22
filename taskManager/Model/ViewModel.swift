50ad13448a2b6b33e94f10972394b172eaa26a4d
David Tonoyan <davidtonoyan10@me.com>
2023-02-22 16:58:49 +0400

    Added ViewModel

---
+++ taskManager/Model/ViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ViewData.swift
+//  taskManager
+//
+//  Created by Давид Тоноян  on 22.02.2023.
+//
+
+import Foundation
+
+enum MainModel {
+	struct ViewData {
+		struct RegularTask {
+			let title: String
+			let taskStatus: TaskStatus
+		}
+
+		struct ImportantTask {
+			let title: String
+			let taskStatus: TaskStatus
+			let isOverdue: Bool
+			let deadLine: String
+			let priority: TaskPriority
+		}
+
+		enum Task {
+			case regularTask(RegularTask)
+			case importantTask(ImportantTask)
+		}
+
+		struct Section {
+			let title: String
+			let tasks: [Task]
+		}
+
+		let tasksBySections: [Section]
+	}
+}
