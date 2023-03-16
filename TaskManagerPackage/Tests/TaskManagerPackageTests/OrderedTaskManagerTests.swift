b01d39d6b26f83a59efb4ae8eb9ddca61207e692
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2023-03-16 17:13:39 +0300

    fixed test

--- TaskManagerPackage/Tests/TaskManagerPackageTests/OrderedTaskManagerTests.swift
+++ TaskManagerPackage/Tests/TaskManagerPackageTests/OrderedTaskManagerTests.swift
@@ -6,15 +6,23 @@
 //
 
 import XCTest
-@testable import TaskManagerPackage
+@testable import taskManager
 
 final class OrderedTaskManagerTests: XCTestCase {
-	var orderedTaskManager: OrderedTaskManager!
-	var taskManager: TaskManager!
+	private var orderedTaskManager: OrderedTaskManager!
+	private var taskManager: TaskManagerMock!
 	
 	override func setUp() {
-		taskManager = TaskManager()
+		taskManager = TaskManagerMock()
 		orderedTaskManager = OrderedTaskManager(taskManager: taskManager)
+		
+		let task1 = Task(title: "Сделать сальтуху")
+		let task2 = Task(title: "Зашить карманы")
+		
+		task1.taskStatus = .completed
+		
+		orderedTaskManager.addTask(task: task1)
+		orderedTaskManager.addTask(task: task2)
 	}
 	
 	override func tearDown() {
@@ -24,14 +32,11 @@ final class OrderedTaskManagerTests: XCTestCase {
 	
 	func test_singleTaskAddition() {
 		
-		// Arrange
 		let tasksCountAfterAdding = taskManager.allTasks().count + 1
 		
-		// Act
 		let task = RegularTask(title: "Сделать сальтуху")
 		orderedTaskManager.addTask(task: task)
 		
-		// Assert
 		XCTAssertEqual(
 			tasksCountAfterAdding,
 			taskManager.allTasks().count,
@@ -41,16 +46,11 @@ final class OrderedTaskManagerTests: XCTestCase {
 	
 	func test_singleTaskRemoval() {
 		
-		// Arrange
-		initialSetup()
-		
 		var tasks = taskManager.allTasks()
 		let tasksCountAfterRemoval = taskManager.allTasks().count - 1
 		
-		// Act
 		orderedTaskManager.removeTask(task: tasks.removeLast())
 		
-		// Assert
 		XCTAssertEqual(
 			tasksCountAfterRemoval,
 			taskManager.allTasks().count,
@@ -60,59 +60,49 @@ final class OrderedTaskManagerTests: XCTestCase {
 	
 	func test_allTasksSorted() {
 		
-		// Arrange
-		initialSetup()
+		let tasks = orderedTaskManager.allTasks()
 		
-		// Act
-		_ = orderedTaskManager.allTasks()
-		
-		// Assert
 		XCTAssertEqual(
-			orderedTaskManager.allTasks()[0].title < orderedTaskManager.allTasks()[1].title,
-			orderedTaskManager.allTasks()[1].title > orderedTaskManager.allTasks()[0].title,
+			tasks[0].title < tasks[1].title,
+			tasks[1].title > tasks[0].title,
 			"Задания не отсортированы"
 		)
 	}
 	
 	func test_ifSorted_byCompletion() {
 		
-		// Arrange
-		initialSetup()
+		let completedTasks = orderedTaskManager.completedTasks()
 		
-		// Act
-		_ = orderedTaskManager.completedTasks()
-		
-		// Assert
-		XCTAssertEqual(
-			orderedTaskManager.allTasks()[0].taskStatus == .completed,
-			orderedTaskManager.allTasks()[1].taskStatus == .planned,
-			"Задания не отсортированы по завершенным"
-		)
+		XCTAssert(completedTasks[0].taskStatus == .completed)
 	}
 	
 	func test_ifSorted_byTasksInProgress() {
 		
-		// Arrange
-		initialSetup()
+		let uncompletedTasks = orderedTaskManager.uncompletedTasks()
 		
-		// Act
-		_ = orderedTaskManager.uncompletedTasks()
-		
-		// Assert
-		XCTAssertEqual(
-			orderedTaskManager.allTasks()[0].taskStatus == .planned,
-			orderedTaskManager.allTasks()[1].taskStatus == .completed,
-			"Список не отсортирован по заданиям в процессе выполнения"
-		)
+		XCTAssert(uncompletedTasks[0].taskStatus == .planned)
 	}
+	
+	private final class TaskManagerMock: ITaskManager {
+		private var tasks = [Task]()
+		
+		func addTask(task: Task) {
+			tasks.append(task)
+		}
+		
+		func removeTask(task: Task) {
+			tasks.removeLast()
+		}
 
-	private func initialSetup() {
-		let task1 = Task(title: "Сделать сальтуху")
-		let task2 = Task(title: "Зашить карманы")
+		func allTasks() -> [Task] {
+			tasks
+		}
 
-		task2.taskStatus = .completed
+		func completedTasks() -> [Task] {
+			tasks.filter { $0.taskStatus == .completed }
+		}
 
-		orderedTaskManager.addTask(task: task1)
-		orderedTaskManager.addTask(task: task2)
+		func uncompletedTasks() -> [Task] {
+			tasks.filter { $0.taskStatus == .planned }}
 	}
 }
