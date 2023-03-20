5545864777d8e919537b723adfcfc691da342484
David Tonoyan <davidtonoyan10@me.com>
2023-03-20 12:28:13 +0400

    Fixed taskManager tests

--- TaskManagerPackage/Tests/TaskManagerPackageTests/TaskManagerTests.swift
+++ TaskManagerPackage/Tests/TaskManagerPackageTests/TaskManagerTests.swift
@@ -1,61 +1,50 @@
+//
+//  SectionManager.swift
+//  taskManager
+//
+//  Created by Давид Тоноян  on 18.02.2023.
+//
+
 import XCTest
 @testable import TaskManagerPackage
 
-class TaskManagerTests: XCTestCase {
-	var taskManager: TaskManager!
+final class TaskManagerTests: XCTestCase {
+	private let taskManager = TaskManager()
 
-	override func setUp() {
-		super.setUp()
-		taskManager = TaskManager()
-	}
-
-	override func tearDown() {
-		taskManager = nil
-		super.tearDown()
-	}
-
-	func test_addTask() {
+	func test_taskManager_addTask_shouldBeSuccesed() {
 		let task = Task(title: "Test Task")
 
 		taskManager.addTask(task: task)
+		let tasks = taskManager.allTasks()
 
-		XCTAssertEqual(taskManager.allTasks().count, 1)
+		XCTAssertFalse(tasks.isEmpty, "Задача не добавлена, список задач пустой")
+		XCTAssertEqual(tasks.count, 1, "Добавлено некорректное кол-во задач - \(tasks.count)")
 	}
 
-	func test_removeTask() {
+	func test_taskManager_removeTask_shouldBeSuccesed() {
 		let task = Task(title: "Test Task")
 		taskManager.addTask(task: task)
 
 		taskManager.removeTask(task: task)
 
-		XCTAssertEqual(taskManager.allTasks().count, 0)
+		let tasks = taskManager.allTasks()
+		XCTAssertTrue(tasks.isEmpty, "Задача не была удалена, список задач не пустой")
 	}
 
-	func test_allTasks() {
+	func test_taskManager_allTasks_shouldBeSuccesed() {
 		let task1 = Task(title: "Test Task 1")
 		let task2 = Task(title: "Test Task 2")
 
 		taskManager.addTask(task: task1)
 		taskManager.addTask(task: task2)
 
-		XCTAssertEqual(taskManager.allTasks().count, 2)
+		let tasks = taskManager.allTasks()
+
+		XCTAssertFalse(tasks.isEmpty, "Список задач пустой")
+		XCTAssertEqual(tasks.count, 2, "Вернулось некорректное кол-во задач - \(tasks.count)")
 	}
 
-	func test_completedTasks() {
-		let task1 = Task(title: "Test Task 1")
-		let task2 = Task(title: "Test Task 2")
-		let task3 = Task(title: "Test Task 3")
-		task1.taskStatus = .completed
-		task3.taskStatus = .completed
-
-		taskManager.addTask(task: task1)
-		taskManager.addTask(task: task2)
-		taskManager.addTask(task: task3)
-
-		XCTAssertEqual(taskManager.completedTasks().count, 2)
-	}
-
-	func test_uncompletedTasks() {
+	func test_taskManager_completedTasks_shouldBeSuccesed() {
 		let task1 = Task(title: "Test Task 1")
 		let task2 = Task(title: "Test Task 2")
 		let task3 = Task(title: "Test Task 3")
@@ -66,6 +55,27 @@ class TaskManagerTests: XCTestCase {
 		taskManager.addTask(task: task2)
 		taskManager.addTask(task: task3)
 
-		XCTAssertEqual(taskManager.uncompletedTasks().count, 1)
+		let completedTasks = taskManager.completedTasks()
+		XCTAssertFalse(completedTasks.isEmpty, "Список выполненных задач пустой")
+		XCTAssertEqual(completedTasks.count, 2, "Вернулось некорректное кол-во задач - \(completedTasks.count)")
+		XCTAssertEqual(completedTasks[0].taskStatus, .completed, "Задача (0) с некорректным статусом")
+		XCTAssertEqual(completedTasks[1].taskStatus, .completed, "Задача (1) с некорректным статусом")
+	}
+
+	func test_taskManager_uncompletedTasks_shouldBeSuccesed() {
+		let task1 = Task(title: "Test Task 1")
+		let task2 = Task(title: "Test Task 2")
+		let task3 = Task(title: "Test Task 3")
+		task1.taskStatus = .completed
+		task3.taskStatus = .completed
+
+		taskManager.addTask(task: task1)
+		taskManager.addTask(task: task2)
+		taskManager.addTask(task: task3)
+
+		let uncompletedTasks = taskManager.uncompletedTasks()
+		XCTAssertFalse(uncompletedTasks.isEmpty, "Список невыполненных задач пустой")
+		XCTAssertEqual(uncompletedTasks.count, 1, "Вернулось некорректное кол-во задач - \(uncompletedTasks.count)")
+		XCTAssertEqual(uncompletedTasks[0].taskStatus, .planned, "Задача (0) с некорректным статусом")
 	}
 }
