703e806b830609d6b77b97b77126ca278ce631cb
David Tonoyan <davidtonoyan10@me.com>
2023-03-30 14:27:57 +0400

    Add sectionManager spy

---
+++ taskManagerTests/TaskListScene/TaskListInteractor/SectionManagerSpy.swift
@@ -0,0 +1,35 @@
+//
+//  SectionManagerSpy.swift
+//  taskManagerTests
+//
+//  Created by Давид Тоноян  on 30.03.2023.
+//
+
+@testable import taskManager
+@testable import TaskManagerPackage
+
+final class SectionForTaskManagerAdapterSpy: ISectionForTaskManagerAdapter {
+	private(set) var isGetSectionsCalled = false
+	private(set) var isGetTasksForSection = false
+	var taskStatus: TaskStatus {
+		testTask.taskStatus
+	}
+
+	private let testTask: Task = .init(title: "Test task")
+
+	func getSections() -> [Section] {
+		isGetSectionsCalled = true
+
+		return [.completed]
+	}
+
+	func getTasksForSection(section: Section) -> [Task] {
+		isGetTasksForSection = true
+		switch section {
+		case .completed:
+			return [testTask]
+		case .uncompleted:
+			return []
+		}
+	}
+}
