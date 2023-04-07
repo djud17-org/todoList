477b348b80c2012cff69d09b2ed9be19c6948b8f
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2023-04-07 10:44:07 +0300

    LoginScene views gained accessibility identifiers

--- taskManager/Scenes/Login/LoginViewController.swift
+++ taskManager/Scenes/Login/LoginViewController.swift
@@ -13,6 +13,12 @@
 import UIKit
 import PinLayout
 
+enum AccessibilityIdentifier: String {
+	case loginTextField
+	case passwordTextField
+	case loginButton
+}
+
 protocol ILoginDisplayLogic: AnyObject {
 
 	/// Функция рендерит вью по модели.
@@ -110,6 +116,7 @@ final class LoginViewController: UIViewController {
 		let textField = UITextField()
 		textField.placeholder = L10n.Authorization.Placeholder.login
 		textField.borderStyle = .roundedRect
+		textField.accessibilityIdentifier = AccessibilityIdentifier.loginTextField.rawValue
 
 		return textField
 	}
@@ -118,6 +125,7 @@ final class LoginViewController: UIViewController {
 		let textField = UITextField()
 		textField.placeholder = L10n.Authorization.Placeholder.password
 		textField.borderStyle = .roundedRect
+		textField.accessibilityIdentifier = AccessibilityIdentifier.passwordTextField.rawValue
 
 		return textField
 	}
@@ -129,6 +137,7 @@ final class LoginViewController: UIViewController {
 		button.backgroundColor = Theme.blue
 		button.setTitleColor(.white, for: .normal)
 		button.setTitleColor(.white.withAlphaComponent(0.5), for: .highlighted)
+		button.accessibilityIdentifier = AccessibilityIdentifier.loginButton.rawValue
 
 		return button
 	}
