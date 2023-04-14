a7e6cff55a5cf68b988b50634da823d4cc7fffb8
Anton Zarichny <109965569+zzzarya@users.noreply.github.com>
2023-04-14 12:02:12 +0300

    TD-30 Реализовать ScreenObject для LoginScene (#77)
    * TD-30/ Add LoginScreenObject
    
    Так же добавил BaseScreenoObject, можно от него наследоваться
    
    * Поправил замечания Давида
    
    * Очередные названия для методов

---
+++ taskManagerTests/Common/BaseScreenObject.swift
@@ -0,0 +1,81 @@
+//
+//  BaseScreenObject.swift
+//  taskManagerTests
+//
+//  Created by Антон Заричный on 10.04.2023.
+//
+
+import XCTest
+
+class BaseScreenObject {
+
+	// MARK: - Dependencies
+
+	let app: XCUIApplication
+
+	// MARK: - Private properties
+
+	private static let defaultTimeout: TimeInterval = 2
+
+	// MARK: - Private elements
+
+	private lazy var alert = app.alerts.firstMatch
+	private lazy var alertButton = alert.buttons.firstMatch
+
+	private lazy var navigationBar = app.navigationBars.firstMatch
+	private lazy var navigationBarButton = navigationBar.buttons.firstMatch
+	private lazy var navigationBarTitle = navigationBar.staticTexts.firstMatch
+
+	// MARK: - Initialization
+
+	init(app: XCUIApplication) {
+		self.app = app
+	}
+
+	// MARK: - ScreenObject Methods
+	/// Функция для проверки UI элемента на сцене
+	@discardableResult
+	func assertElement(_ element: XCUIElement, _ predicates: [Predicate], timeout: TimeInterval = defaultTimeout) -> Self {
+		let expectation = XCTNSPredicateExpectation(
+			predicate: NSPredicate(format: predicates.map { $0.format }.joined(separator: " AND ")),
+			object: element
+		)
+
+		guard XCTWaiter.wait(for: [expectation], timeout: timeout) == .completed else {
+			XCTFail("[\(self)] Элемент \(element.description) не соответствует ожиданию: \(predicates.map { $0.format })")
+			return self
+		}
+		return self
+	}
+	/// Функция для нажатия кнопки
+	@discardableResult
+	func tapOnElement(_ element: XCUIElement, timeout: TimeInterval = BaseScreenObject.defaultTimeout) -> Self {
+		assertElement(element, [.isHittable], timeout: timeout)
+		element.tap()
+
+		return self
+	}
+	/// Функция для возвращения на предыдущий экран
+	@discardableResult
+	func backOnPreviousScene(timeout: TimeInterval = BaseScreenObject.defaultTimeout) -> Self {
+		tapOnElement(navigationBarButton, timeout: timeout)
+
+		return self
+	}
+}
+
+// MARK: - Keyboard
+
+/// Управление клавиатурой
+extension BaseScreenObject {
+	enum Lang: String {
+		case eng = "English (US)"
+		case rus = "Русская"
+		case emoji = "Emoji"
+	}
+
+	func setKeyboard(language: Lang) {
+		app.buttons["Next keyboard"].press(forDuration: 0.5)
+		app.tables["InputSwitcherTable"].staticTexts[language.rawValue].tap()
+	}
+}
